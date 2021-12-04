6ebf792cd6ce06b9fafe5e3275598f8141526664
ringyaphy <ringyaphy21@navgurukul.org>
2021-12-05 02:10:40 +0530

    first commit

---
+++ if_else.py
@@ -0,0 +1,7 @@
+day =input ("enter the  day")
+if day =="monday":
+    print("hope you have a great day")
+elif day=="friday"or day=="sat"or day=="sunday":
+      print("enjoy the weekend")
+else:
+    print("working day")
